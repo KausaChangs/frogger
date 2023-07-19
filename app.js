0753c584a49df45ab86e16a4fc04381234aa2f2a
KausaChangs <changayakausa@gmail.com>
2023-07-19 19:18:07 +0200

    created a move frog function

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const timeLeftDisplay = document.querySelector("#time-left");
 const resultDisplay = document.querySelector("#result");
 const startPauseButton = document.querySelector("#start-pause-button");
 const squares = document.querySelector("#.grid div");
+
+function moveFrog() {
+  console.log("moved");
+}
