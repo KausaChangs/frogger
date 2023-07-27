509f0be38af88ee36f1ca4e36fb4ef77e1bea675
KausaChangs <changayakausa@gmail.com>
2023-07-27 08:49:10 +0200

    need to sort out weird behavior in app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const timeLeftDisplay = document.querySelector("#time-left");
 const resultDisplay = document.querySelector("#result");
 const startPauseButton = document.querySelector("#start-pause-button");
-const squares = document.querySelector("#.grid div");
+const squares = document.querySelectorAll(".grid div");
 
 function moveFrog() {
   console.log("moved");
 }
+
+document.addEventListener("keyup", moveFrog);
